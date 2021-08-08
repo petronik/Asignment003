aa245de9a9104484641da8d1c349d5210c3a3870
Petronik <petronik@ukr.net>
2021-08-08 15:45:53 -0700

    added IComparable and Sort Method

--- Asignment003/SerializedFile.cs
+++ Asignment003/SerializedFile.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Asignment003
 {
-    public class SerializedFile 
+    public class SerializedFile : IComparable<SerializedFile>
     {
         public long Size { get; set; }
         public string Name { get; set; }
+
+        public int CompareTo(SerializedFile other)
+        {
+            if (other == null)
+                return 1;
+            else
+                return Size.CompareTo(other.Size);
+        }
     }
 }
